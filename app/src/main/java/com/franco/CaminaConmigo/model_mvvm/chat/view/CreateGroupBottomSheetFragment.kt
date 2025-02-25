81d1eea47bbfec283bb8eee60d8dfa022df8ba36
TheSniperS2 <francomgbkn@gmail.com>
2025-02-25 02:21:43 -0300

    Opcion de crear grupo con imagen

--- app/src/main/java/com/franco/CaminaConmigo/model_mvvm/chat/view/CreateGroupBottomSheetFragment.kt
+++ app/src/main/java/com/franco/CaminaConmigo/model_mvvm/chat/view/CreateGroupBottomSheetFragment.kt
@@ -1,7 +1,10 @@
 package com.franco.CaminaConmigo.model_mvvm.chat.view
 
+import android.app.Activity
 import android.app.Dialog
+import android.content.Intent
 import android.content.res.Resources
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -19,10 +22,12 @@ import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+import java.util.UUID
 
 class CreateGroupBottomSheetFragment : BottomSheetDialogFragment() {
 
@@ -31,6 +36,7 @@ class CreateGroupBottomSheetFragment : BottomSheetDialogFragment() {
     private val viewModel: ChatViewModel by viewModels()
     private val db = FirebaseFirestore.getInstance()
     private val auth = FirebaseAuth.getInstance()
+    private var groupImageUri: Uri? = null
 
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
 
@@ -53,6 +59,13 @@ class CreateGroupBottomSheetFragment : BottomSheetDialogFragment() {
 
         loadFriends(friendsAdapter)
 
+        binding.imageViewGroup.setOnClickListener {
+            // Lógica para seleccionar una imagen del dispositivo
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 1)
+        }
+
         binding.buttonCreateGroup.setOnClickListener {
             val selectedFriends = friendsAdapter.getSelectedFriends()
             val groupName = binding.editTextGroupName.text.toString().trim()
@@ -61,12 +74,21 @@ class CreateGroupBottomSheetFragment : BottomSheetDialogFragment() {
             } else if (selectedFriends.size < 2) {
                 Toast.makeText(requireContext(), "Debes seleccionar al menos 2 amigos para crear un grupo.", Toast.LENGTH_SHORT).show()
             } else {
-                createGroup(selectedFriends, groupName)
+                createGroup(selectedFriends, groupName, groupImageUri)
+                dismiss() // Cerrar la ventana inmediatamente
             }
         }
 
         binding.btnCerrar.setOnClickListener {
             dismiss()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            groupImageUri = data?.data
+            binding.imageViewGroup.setImageURI(groupImageUri)
         }
     }
 
@@ -93,11 +115,11 @@ class CreateGroupBottomSheetFragment : BottomSheetDialogFragment() {
         }
     }
 
-    private fun createGroup(selectedFriends: List<Friend>, groupName: String) {
+    private fun createGroup(selectedFriends: List<Friend>, groupName: String, groupImageUri: Uri?) {
         val currentUserId = auth.currentUser?.uid ?: return
         val groupUserIds = selectedFriends.map { it.id } + currentUserId
 
-        val groupData = mapOf(
+        val groupData = hashMapOf(
             "adminIds" to listOf(currentUserId),
             "lastMessage" to "Has sido añadido al grupo $groupName",
             "lastMessageTimestamp" to com.google.firebase.Timestamp.now(),
@@ -109,13 +131,31 @@ class CreateGroupBottomSheetFragment : BottomSheetDialogFragment() {
         )
 
         db.collection("chats").add(groupData)
-            .addOnSuccessListener {
-                Toast.makeText(requireContext(), "Grupo creado exitosamente.", Toast.LENGTH_SHORT).show()
-                dismiss()
+            .addOnSuccessListener { documentReference ->
+                // Subir la imagen en segundo plano y actualizar la URL en Firestore
+                if (groupImageUri != null) {
+                    uploadGroupImage(documentReference.id, groupImageUri)
+                }
             }
             .addOnFailureListener { e ->
                 Toast.makeText(requireContext(), "Error al crear el grupo: ${e.message}", Toast.LENGTH_SHORT).show()
             }
+    }
+
+    private fun uploadGroupImage(groupId: String, imageUri: Uri) {
+        lifecycleScope.launch {
+            try {
+                val storageRef = FirebaseStorage.getInstance().reference.child("group_images/${UUID.randomUUID()}")
+                storageRef.putFile(imageUri).await()
+                val downloadUrl = storageRef.downloadUrl.await()
+                db.collection("chats").document(groupId).update("groupURL", downloadUrl.toString())
+            } catch (e: Exception) {
+                // Manejar error en la carga de la imagen
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(requireContext(), "Error al subir la imagen del grupo.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
